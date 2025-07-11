eec86b5529d06e4c4225772731d68c076b8135eb
David Wong <mingtak.wong1986@gmail.com>
2025-07-11 11:28:49 -0400

    Kicking start of writing a blackjack game

---
+++ Blackjack/blackjack_simulator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 11 10:39:48 2025
+
+@author: mingt
+"""
+
+""" Intend to write a python application that simulates a multiplayer game;
+will start with single-player game against a dealer and supports basic game rule 
+including hit, stand, blackjack rules, dealer rules
+"""
+
+"""
+Create a deck
+"""
+
+"""
+Calculate hand value
+"""
+
+"""
+Simulate a game
+"""
+
