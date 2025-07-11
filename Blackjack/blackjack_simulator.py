3b4fb146e39ca6dcb74c8769b204b203e297beeb
David Wong <mingtak.wong1986@gmail.com>
2025-07-11 11:43:00 -0400

    Provide brief specifications

--- Blackjack/blackjack_simulator.py
+++ Blackjack/blackjack_simulator.py
@@ -11,14 +11,21 @@ including hit, stand, blackjack rules, dealer rules
 """
 
 """
-Create a deck
+Create a deck:
+    # INPUT: None
+    # OUTPUT: An array of 52 cards (4 suits, 13 cards each)
 """
 
 """
-Calculate hand value
+Calculate hand value:
+    # INPUT: An array of x cards (from 1 to x - 1 as long as sum before draw is less than 21 or 5 for some variant) 
+    # OUTPUT: Calculate hand value given input
+
 """
 
 """
 Simulate a game
+    # INPUT: An array of x and y cards (x for player, y for dealer)
+    # OUTPUT: return Win, draw, loss 
 """
 
